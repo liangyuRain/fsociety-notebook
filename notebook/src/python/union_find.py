f8d70a0165d929e4ef05c83822d1e72acc35ea40
Leo Du <dul2@uw.edu>
2017-10-29 04:49:40 -0700

    union find python

---
+++ notebook/src/python/union_find.py
@@ -0,0 +1,36 @@
+
+class DisjointSet(object):
+
+    def __init__(self, capacity):
+        self.forest = list(range(capacity))
+
+    def find(self, x):
+        if self.forest[x] != x:
+            self.forest[x] = self.find(self.forest[x]) # path compression
+        return self.forest[x]
+
+
+    def union(self, x, y):
+        """
+        :param x: disjoint set element
+        :param y: disjoint set element
+        :return: True on success (merged two sets),
+                 False otherwise (already in one set)
+        """
+        if self.find(x) == self.find(y):
+            return False
+        else:
+            self.forest[self.find(x)] = self.find(y)
+            return True
+
+
+if __name__ == '__main__':
+    ds = DisjointSet(10)
+    print(ds.find(1))
+    print(ds.find(2))
+    ds.union(1,2)
+    print(ds.find(1))
+    print(ds.find(2))
+    ds.union(3,4)
+    print(ds.find(3))
+    print(ds.find(4))
