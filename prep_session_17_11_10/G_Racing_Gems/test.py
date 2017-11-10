4fcac6984dfd6fd0479b30cc4017e9fddb78bf4b
Leo Du <dul2@uw.edu>
2017-11-10 15:01:26 -0800

    test suites

--- prep_session_17_11_10/G_Racing_Gems/test.py
+++ prep_session_17_11_10/G_Racing_Gems/test.py
@@ -1,9 +1,9 @@
 import subprocess
 
 if __name__ == '__main__':
-    # test_cases = list(range(1000,1009))
-    test_cases = list(range(0,3))
-    # test_cases.extend(range(2000,2006))
+    test_cases = list(range(1000,1009))
+    # test_cases = list(range(0,3))
+    test_cases.extend(range(2000,2006))
 
     for test_case in test_cases:
         subprocess.run(['cat', 'output/RacingGems-%04d.out' % test_case])
