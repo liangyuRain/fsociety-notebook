f1f046b519f40e1fdacfa5ddd9bc4688f68bd72e
Leo Du <dul2@uw.edu>
2017-11-10 12:53:04 -0800

    fix time problem

--- prep_session_17_11_09/K_Tournament/solution.py
+++ prep_session_17_11_09/K_Tournament/solution.py
@@ -4,31 +4,23 @@ from math import gcd
 
 def solution(k, r):
     expectation = 0
-    total = 2**k
+    total = 1 << k
+    below = total - r
+    probability = 1.0
+    choices = total - 1
 
-    numerator = total - k
-    denominator = total - 1
-    increment = numerator / denominator
-
-    prev_num = total - k
-    prev_den = total - 1
-
-    level = 2
-
-    while numerator > 0:
-        if increment >= 0.00001:
-            expectation += numerator / denominator
-        else:
+    for i in range(k):
+        for j in range((1<<i) - 1, (1<<(i+1)) - 1):
+            probability *= (below - j) / (choices - j)
+        if probability == 0.0:
             break
-        increment *= reduce(lambda x, y : x*y, range(prev_num-level, prev_num)) / reduce(lambda x, y : x*y, range(prev_den-level, prev_den))
-        prev_num -= level
-        prev_den -= level
-        level *= 2
+        else:
+            expectation += probability
 
     return expectation
 
 
 if __name__ == '__main__':
-    f = sys.stdin
-    k, r, = list(map(int, f.readline().split(" ")))
-    print(solution(k, r))
+
+    k, r, = list(map(int, input().split(" ")))
+    print("%.5lf" % solution(k, r))
