ac1251a4e5b070a54056c005ead77b355b561d7f
Leo Du <dul2@uw.edu>
2017-11-10 14:29:51 -0800

    test for G

---
+++ prep_session_17_11_10/G_Racing_Gems/test.py
@@ -0,0 +1,10 @@
+import subprocess
+
+if __name__ == '__main__':
+    # test_cases = list(range(1000,1009))
+    test_cases = list(range(0,3))
+    # test_cases.extend(range(2000,2006))
+
+    for test_case in test_cases:
+        subprocess.run(['cat', 'output/RacingGems-%04d.out' % test_case])
+        subprocess.run(['time', 'java', 'Problem_G'], stdin=open('input/RacingGems-%04d.in' % test_case))
