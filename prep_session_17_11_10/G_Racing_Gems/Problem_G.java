05ac37c3f1d6786ddb73ece3d548f7fc725dda9f
YINGRU FENG <yingru97@cs.washington.edu>
2017-11-10 15:44:38 -0800

    problem G

--- prep_session_17_11_10/G_Racing_Gems/Problem_G.java
+++ prep_session_17_11_10/G_Racing_Gems/Problem_G.java
@@ -43,21 +43,18 @@ public class Problem_G {
 
         for (int i = 0; i < n; i++) {
             long max = 1;
-            HashSet<Integer> task = new HashSet<Integer>();
+            TreeSet<Integer> task = new TreeSet<Integer>();
             for (int j = 0; j < i; j++) {
                 task.add(j);
             }
             while (!task.isEmpty()) {
-                int j = task.iterator().next();
+                int j = task.first();
                 if ((Math.abs(gems.get(i).x - gems.get(j).x) * r) <= Math.abs(gems.get(i).y - gems.get(j).y)) {
                     if (maxValues.get(j) + 1 > max) {
                         max = maxValues.get(j) + 1;
                     }
-                    for (int k : visited.get(j)) {
-                        task.remove(k);
-                        visited.get(i).add(k);
-                    }
-                    visited.get(i).add(j);
+                    task.removeAll(visited.get(j));
+                    visited.get(i).addAll(visited.get(j));
                 }
                 task.remove(j);
             }
